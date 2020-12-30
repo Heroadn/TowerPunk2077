258fa5cc01ebafe0746b4adc7b58e0d429e9b5df
Heroadn <benbenjamin554@gmail.com>
2020-12-30 15:12:53 -0300

    fix

--- Assets/Scripts/Inventory/InventorySlot.cs
+++ Assets/Scripts/Inventory/InventorySlot.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 
 
-public class InventoryButton : MonoBehaviour{
+public class InventorySlot{
     public InventoryItem item;
     public GameObject button;
 
--- Assets/Scripts/Player/PlayerInventory.cs
+++ Assets/Scripts/Player/PlayerInventory.cs
@@ -12,16 +12,16 @@ public class PlayerInventory : MonoBehaviour{
     public GameObject inventoryGui;
 
     private InventoryComponent inventoryComponent;
-    private InventoryButton[] slots;
+    private InventorySlot[] slots;
 
     void Start(){
         //obtendo componente de inventario
         inventoryComponent = GetComponent<InventoryComponent>();
-        inventoryComponent.getInventory().add(new Wood(), new ArmadilhaUrso());
+        inventoryComponent.getInventory().add(new Wood(), new Wood(), new ArmadilhaUrso());
 
         //tamanho de slots e inicializando array
-        size_slots = inventoryComponent.getInventorySize();
-        slots      = new InventoryButton[size_slots];
+        size_slots = 14;
+        slots      = new InventorySlot[size_slots];
 
         //Janela do inventario desativada no inicio
         inventoryGui.SetActive(inventoryEnabled);
@@ -29,7 +29,7 @@ public class PlayerInventory : MonoBehaviour{
         //Inicializando slots vazios
         for (int i=0; i < size_slots; i++)
         {
-            slots[i] = new InventoryButton();
+            slots[i] = new InventorySlot();
             slots[i].button = slotHolder.transform.GetChild(i).gameObject;
 
             //prenchendo de items e icones
@@ -44,7 +44,29 @@ public class PlayerInventory : MonoBehaviour{
 
     void Update(){
         if(Input.GetKeyDown(KeyCode.I)){
+            loadInventory();
             inventoryGui.SetActive(inventoryEnabled = !inventoryEnabled);
+        }
+
+        //adicionando ao inventario
+        if(Input.GetKeyDown(KeyCode.V)){
+            inventoryComponent.getInventory().add(new Wood());
+        }
+    }
+
+    //Todo: simplificar
+    void loadInventory(){
+        for (int i=0; i < size_slots; i++)
+        {
+            //prenchendo de items e icones
+            if(i < inventoryComponent.getInventorySize())
+            {
+                Debug.Log("I: "+i);
+                Debug.Log("S: "+inventoryComponent.getInventorySize());
+                slots[i].item = inventoryComponent.getItem(i);
+                slots[i].button.GetComponent<Image>().sprite 
+                        = Icons.sprites[slots[i].item.icon_id];
+            }
         }
     }
 }
