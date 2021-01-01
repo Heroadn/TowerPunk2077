5ceacc52d541183e9eb6cc0c7f25e23f3ad8eb73
Heroadn <benbenjamin554@gmail.com>
2021-01-01 10:07:20 -0300

    fix

--- Assets/Scripts/Event/CatchMe.cs
+++ Assets/Scripts/Event/CatchMe.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Follow : MonoBehaviour
+public class CatchMe : MonoBehaviour
 {
     public GameObject player;
     public int distancia = 10;
