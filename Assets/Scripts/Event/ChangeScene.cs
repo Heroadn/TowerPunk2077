9c555b401cfe7f5bc55a6485598b2812d008e759
Heroadn <benbenjamin554@gmail.com>
2021-01-05 20:07:59 -0300

    changelog e testa o 3D ;)

---
+++ Assets/Scripts/Event/ChangeScene.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeScene : MonoBehaviour
+{
+    public LevelDatabase database;
+    public GameObject player;
+    public int levelIndex;
+
+    void OnCollisionEnter(Collision collision){
+        if(collision.gameObject == player)
+        {
+            database.loadLevel(levelIndex);
+        }
+            
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/Level/LevelDatabase.cs
+++ Assets/Scripts/Level/LevelDatabase.cs
@@ -2,11 +2,56 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 [CreateAssetMenu(fileName = "New Database", menuName = "Level/New Database")]
 public class LevelDatabase : ScriptableObject{
 
     public List<Level> levels = new List<Level>();
+    public int indexOfCurrentLevel = 0;
 
-    
+    public void loadLevel(string level){
+        SceneManager.LoadScene(level);
+    }
+
+    public void loadCurrentLevel(){
+        if(indexOfCurrentLevel >= levels.Count)
+        {
+            Debug.Log("Index out of bounds");
+            return;
+        }
+
+        SceneManager.LoadScene(levels[indexOfCurrentLevel].name);
+    }
+
+    public void loadLevel(int index){
+        if(index >= levels.Count)
+        {
+            Debug.Log("Index out of bounds");
+            return;
+        }
+
+        indexOfCurrentLevel = index;
+        SceneManager.LoadScene(levels[index].name);
+    }
+
+    public void loadNextLevel(){
+        if(++indexOfCurrentLevel >= levels.Count)
+        {
+            Debug.Log("++indexOfCurrentLevel out of bounds");
+            return;
+        }
+
+        SceneManager.LoadScene(levels[indexOfCurrentLevel].name);
+    }
+
+    public void loadPrevLevel(){
+        if(--indexOfCurrentLevel >= levels.Count)
+        {
+            Debug.Log("--indexOfCurrentLevel out of bounds");
+            return;
+        }
+
+        SceneManager.LoadScene(levels[indexOfCurrentLevel].name);
+    }
 }
