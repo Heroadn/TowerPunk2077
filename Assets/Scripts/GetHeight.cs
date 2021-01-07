91866c1f56a0ac651563d927067ab6673c5eaf1f
Oicanji <51388779+Oicanji@users.noreply.github.com>
2021-01-07 14:56:08 -0300

    Mudanças no 2D

---
+++ Assets/Scripts/GetHeight.cs
@@ -0,0 +1,29 @@
+﻿ /*
+    Author: Ignacio
+*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetHeight : MonoBehaviour {
+ 
+     public GameObject ball;
+     float width;
+     float height;
+     GameObject myball;
+     RectTransform rt;
+     
+     // Use this for initialization
+     void Start () {
+ 
+         myball = (GameObject)Instantiate(ball);
+         rt = (RectTransform)myball.transform;
+         
+         width = rt.rect.width;
+         height = rt.rect.height;                
+
+         Debug.Log("width: "+width+" | Height: ");
+         
+         
+         }
+ }
